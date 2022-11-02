0cabc9867d195452086912d1030e77dc2bbe6633
Ron Vaknin <102041936+Ronvak@users.noreply.github.com>
2022-11-02 18:53:51 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-2"
+}
+# Create a VPC
+resource "aws_vpc" "Ron-Vaknin-dev-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Ron-Vaknin-dev-vpc"
+  }
+}
+resource "aws_subnet" "Ron_Vaknin-k8s-subnet" {
+  vpc_id     = "vpc-019fc11d376c3ad47"
+  cidr_block = "10.0.0.0/27"
+
+ tags = {
+    Name = "Ron_Vaknin-k8s-subnet"
+  }
+}
